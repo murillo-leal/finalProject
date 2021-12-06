d8fa096f959f80d2a0ef1be0a0b38aac3d5e7951
Murillo Leal <murilloadcl@msn.com>
2021-12-06 00:46:39 -0300

    adding models, db and error pages

--- shelbyacai/__init__.py
+++ shelbyacai/__init__.py
@@ -1,7 +1,34 @@
 # shelbyacai/__init__.py
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_migrate import Migrate
+from flask_login import LoginManager, login_manager
 
 app = Flask(__name__)
 
+
+## Database Setup###
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+############################
+## Login Configs
+
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+
+###########################
+
+
 from shelbyacai.core.views import core
+from shelbyacai.error_pages.handlers import error_pages
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
---
+++ shelbyacai/models.py
@@ -0,0 +1,92 @@
+#models.py
+from shelbyacai import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String, nullable=False)
+    cellphone = db.Column(db.String, nullable=False)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    admin = db.Column(db.Boolean, nullable=True)
+
+    orders = db.relationship('Order', backref='OrderId', lazy=True)
+
+    def __init__(self, email, password, username, address, cellphone):
+        self.email = email
+        self.address = address
+        self.password_hash = generate_password_hash(password)
+        self.username = username
+        self.cellphone = cellphone
+
+    def check_password(self, password):
+        return  check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f"Username{self.username}"          
+
+class Order(db.Model):
+    users = db.relationship(User)
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    products = db.relationship('Product', backref="productId", lazy='dynamic')    
+    value = db.Column(db.String, nullable=False)
+    payment = db.Column(db.String, nullable=False)
+
+    def __init__(self,user_id, products, payment, value):
+        payment = payment
+        user_id = user_id
+        products = products
+        value = value
+
+    def __repr__(self):
+        return f"Order Id: {self.id} -- Date: {self.date} --- Products {self.products}"
+
+
+class Toppings(db.Model):
+    __tablename__ = 'toppings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    value = db.Column(db.String, nullable=False)
+    toppingType = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, name, value, toppingType):
+        self.name = name
+        self.value = value
+        self.toppingType = toppingType
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    size = db.Column(db.String, nullable=False)
+    value = db.Column(db.Integer, nullable=False)
+    toppings = db.relationship(Toppings)
+
+    def calculateFinalValue(self, toppings):
+        return self.value + toppings.value
+
+    def __init__(self, toppings, name, size, value, finalvalue):
+        self.name = name
+        self.toppings = toppings
+        self.size = size
+        self.value = value        
+        self.finalvalue = finalvalue   
+
+    def __repr__(self):
+        return f"Product Id: {self.id} -- Product Size: {self.size} --- Product Value {self.value} ---  Product Toppings: {self.toppings}--- Toppings Value:{self.toppings.finalvalue}  --- Final Value:{self.finalvalue}"
+
